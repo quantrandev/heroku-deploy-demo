a5c72593c04a39864d94a789419ddd6a6ca2e851
Quan Tran <quantran081196@gmail.com>
2018-05-09 13:52:40 +0700

    add login component and login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './authentication/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,7 +6,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-const appRoutes: Routes = [{ path: '', component: HomeComponent }];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
