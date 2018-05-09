2b9e901e308fe93e7d540e83b16dfceef90448fa
Quan Tran <quantran081196@gmail.com>
2018-05-09 15:10:55 +0700

    fixed server routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/heroku-deploy-demo/'));
 
-app.get('/', (req, res) => {
+app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/heroku-deploy-demo/index.html');
 });
 
