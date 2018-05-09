2f20d91c769213124abef652368aaf0c1948480b
Quan Tran <quantran081196@gmail.com>
2018-05-09 15:06:10 +0700

    fixed static href

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/heroku-deploy-demo/'));
 
-app.all('*', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/heroku-deploy-demo/index.html');
 });
 
